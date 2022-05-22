e7034ddcf75839dc53bfa60a7e8c30ce579f2674
Vivek Shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2022-05-22 15:00:54 +0530

    refactor: Rename main.py to app.py and remove FLASK_APP env

--- backend/flaskbringup.ps1
+++ backend/flaskbringup.ps1
@@ -1,5 +1,4 @@
 Write-Output "Bringing up flask server!";
 
-$env:FLASK_APP = "main.py";
 $env:FLASK_ENV = "development";
 iex "flask run";
--- backend/flaskbringup.sh
+++ backend/flaskbringup.sh
@@ -8,8 +8,6 @@ pip3 --version
 
 pip3 install -r requirements.txt
 
-export FLASK_APP="main.py"
-
 export FLASK_ENV="development"
 
 flask run
