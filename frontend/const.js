d241b7b66f7b5f591d4f5e2d2c04480e747d05e4
Vivek Shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2022-06-08 20:20:01 +0530

    chore: Update aws lambda endpoint

--- frontend/const.js
+++ frontend/const.js
@@ -1,4 +1,4 @@
 const MAX_INPUT_SIZE = 2560000;
-const BASE_URL = 'https://dpkhcwfw4u6d2ujn3ktmvwfgjm0catjz.lambda-url.ap-south-1.on.aws/json-to-csv'
+const BASE_URL = 'https://evf5ouvlgdqeex5x2ywh4zqpyq0dgrsz.lambda-url.ap-south-1.on.aws/json-to-csv'
 const DOWNLOAD_URL = `${BASE_URL}/convert?format=csv&skip-flattening=true&download=true`
 const CONVERT_URL = `${BASE_URL}/convert?format=json`
